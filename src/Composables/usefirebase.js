5ba27944975a7a7e21fb455d94ddbf3f53d857ae
Haylie-g <hguzma15@stu.southtexascollege.edu>
2024-11-17 12:53:26 -0600

    Initial commit

---
+++ src/Composables/usefirebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBy5nVuBX2l_7_aZVHuoUQESOtfmVrdzgM",
+  authDomain: "company-directory-316ca.firebaseapp.com",
+  projectId: "company-directory-316ca",
+  storageBucket: "company-directory-316ca.appspot.com",
+  messagingSenderId: "871313667354",
+  appId: "1:871313667354:web:a80325aa143e6c9f98a8a3",
+
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+
+
---
+++ src/components/MainCardSingle.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { faker } from '@faker-js/faker'
+const firstname = faker.person.firstName()
+const lastname = faker.person.lastName()
+const fullname = faker.person.fullName({ firstname: firstname, lastname: lastname })
+const job_title = faker.person.jobTitle()
+const bio = faker.lorem.lines(2)
+const username = faker.internet.userName({ firstname: firstname, lastname: lastname }).toLowerCase()
+</script>
+
+<template>
+  <!-- {{ fullname }}<br /> -->
+  <RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+    <div class="border border-gray-400 rounded-lg shadow bg-gray-200 h-full"></div>
+    <div>
+        <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category:'cat'})" class="rounded-t-xl w-full h-48 object-cover" />
+        <div class="p-4">
+           <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
+            <p class="mb-3 font-normal text-gray-700">{{ bio }}</p> 
+        </div>
+    </div>
+    </RouterLink>
+</template>
